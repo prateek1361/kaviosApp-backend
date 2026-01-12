1efc5e13ccefa367361f59176629c318bc6aa97e
Prateek Sharma <prateek20091234@gmail.com>
2026-01-12 17:26:34 +0530

    fix3

--- config/passport.js
+++ config/passport.js
@@ -1,24 +1,28 @@
-import passport from "passport";
-import { Strategy as GoogleStrategy } from "passport-google-oauth20";
-import User from "../models/User.js";
-import { v4 as uuid } from "uuid";
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const User = require("../models/User");
+const { v4: uuid } = require("uuid");
 
 passport.use(
   new GoogleStrategy(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "/auth/google/callback"
+      callbackURL: "/auth/google/callback",
     },
     async (_, __, profile, done) => {
-      const email = profile.emails[0].value;
-      let user = await User.findOne({ email });
+      try {
+        const email = profile.emails[0].value;
+        let user = await User.findOne({ email });
 
-      if (!user) {
-        user = await User.create({ userId: uuid(), email });
+        if (!user) {
+          user = await User.create({ userId: uuid(), email });
+        }
+
+        done(null, user);
+      } catch (error) {
+        done(error, null);
       }
-
-      done(null, user);
     }
   )
 );
