244c80d253a5becca82c62ede392f12e7d8b6b0d
Prateek Sharma <prateek20091234@gmail.com>
2026-01-16 04:33:20 +0530

    last push

--- models/Image.js
+++ models/Image.js
@@ -1,39 +1,16 @@
 const mongoose = require("mongoose");
 
 const imageSchema = new mongoose.Schema({
-  imageId: {
-    type: String,
-    required: true,
-  },
-
-  albumId: {
-    type: String,
-    required: true,
-  },
-
+  imageId: { type: String, required: true },
+  albumId: { type: String, required: true },
   name: String,
-
-  imageUrl: {
-    type: String,
-    required: true,
-  },
-
-  tags: [String],          
-  people: [String],       
-
-  isFavorite: {
-    type: Boolean,
-    default: false,
-  },
-
-  comments: [String],
-
+  imageUrl: { type: String, required: true },
+  tags: { type: [String], default: [] },
+  person: { type: String, default: "" }, // <-- changed from people
+  isFavorite: { type: Boolean, default: false },
+  comments: { type: [String], default: [] },
   size: Number,
-
-  uploadedAt: {
-    type: Date,
-    default: Date.now,
-  },
+  uploadedAt: { type: Date, default: Date.now },
 });
 
 module.exports = mongoose.model("Image", imageSchema);
